e77408e8e132268f5eeb79e18f90f81e05f1c592
Anisja <anisja.mayer@stud.unibas.ch>
2024-03-16 17:55:22 +0100

    implemented unit test for Dice class

---
+++ src/test/java/server/gamelogic/DiceTest.java
@@ -0,0 +1,71 @@
+package server.gamelogic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @Test
+    @DisplayName("Checks if dice gets rolled correctly and all the variables get adjusted.")
+    void rollDiceTest(){
+        // three dices, one gets rolled three times, one once, the other doesn't
+        // should return three rolls at most
+        // should return values between 1 and 6
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        Dice dice3 = new Dice();
+        dice2.rollDice();
+        dice3.rollDice();
+        dice3.rollDice();
+        dice3.rollDice();
+
+        assertAll(() -> assertTrue(dice1.rollDice()),
+                () -> assertTrue(dice2.rollDice()),
+                () -> assertFalse(dice3.rollDice()),
+                () -> assertFalse(dice1.getRolledStatus()),
+                () -> assertTrue(dice2.getRolledStatus()),
+                () -> assertTrue(dice3.getRolledStatus()),
+                () -> assertEquals(0, dice1.getNumberOfRolls()),
+                () -> assertEquals(1, dice2.getNumberOfRolls()),
+                () -> assertEquals(3, dice3.getNumberOfRolls()),
+                () -> assertFalse(dice1.getSavingStatus()),
+                () -> assertFalse(dice2.getSavingStatus()),
+                () -> assertTrue(dice3.getSavingStatus()),
+                () -> assertEquals(0, dice1.getDiceValue()),
+                () -> assertTrue(dice2.getDiceValue() <= 6 && dice2.getDiceValue() >= 1),
+                () -> assertTrue(dice3.getDiceValue() <= 6 && dice3.getDiceValue() >= 1)
+                );
+    }
+
+    @Test
+    @DisplayName("Checks if you can roll dice after saving it.")
+    void saveDiceTest(){
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        dice2.saveDice();
+
+        assertAll(() -> assertFalse(dice1.getSavingStatus()),
+                () -> assertTrue(dice2.getSavingStatus()),
+                () -> assertTrue(dice1.rollDice()),
+                () -> assertFalse(dice2.rollDice())
+        );
+    }
+
+    @Test
+    @DisplayName("Checks if reset function works.")
+    void resetDiceTest(){
+        Dice dice = new Dice();
+        dice.rollDice();
+        dice.resetDice();
+
+        assertAll(() -> assertTrue(dice.getRolledStatus()),
+                () -> assertFalse(dice.getSavingStatus()),
+                () -> assertEquals(0, dice.getNumberOfRolls()),
+                () -> assertEquals(0, dice.getDiceValue()),
+                () -> assertTrue(dice.rollDice())
+        );
+
+    }
+}
