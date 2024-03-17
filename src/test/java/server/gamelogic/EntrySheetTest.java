a24dbe595bc511fd8f9ada85dffeb944c610c667
Anisja <anisja.mayer@stud.unibas.ch>
2024-03-17 19:52:57 +0100

    added unit test for EntrySheet

---
+++ src/test/java/server/gamelogic/EntrySheetTest.java
@@ -0,0 +1,41 @@
+package server.gamelogic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntrySheetTest {
+    // default entry sheet
+    EntrySheet entrySheet1 = new EntrySheet("uniqueName001");
+    EntrySheet entrySheet2 = new EntrySheet("uniqueName002");
+    int[] defaultEntrySheetValues = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    String[] defaultEntrySheetNames = {"ones", "twos", "threes", "fours", "fives", "sixes", "threeOfAKind", "fourOfAKind", "fullHouse", "smallStraight", "largeStraight", "kniffeliger", "chance"};
+
+    @Test
+    @DisplayName("Tests if default of entry sheet is correct, so if resetEntrySheet() works and if methods getUsername(), getTotalPoints()")
+    void getTest() {
+        try {
+            entrySheet2.addEntry(new Entry("ones", 1));
+        } catch (Exception e){
+            new Exception("Could not find the entry name.");
+        }
+        assertAll(() -> assertTrue(Arrays.equals(defaultEntrySheetNames, entrySheet1.getEntryNames())),
+                () -> assertTrue(Arrays.equals(defaultEntrySheetValues, entrySheet1.getEntryValues())),
+                () -> assertEquals("uniqueName001", entrySheet1.getUsername()),
+                () -> assertEquals("uniqueName002", entrySheet2.getUsername()),
+                () -> assertFalse("username".equals(entrySheet1.getUsername())),
+                () -> assertEquals(1, entrySheet2.getEntryValues()[0]),
+                () -> assertEquals(0, entrySheet1.getTotalPoints()),
+                () -> assertEquals(1, entrySheet2.getTotalPoints())
+        );
+
+        // resets entrySheet2 to default
+        entrySheet2.resetEntrySheet();
+        assertAll(() -> assertTrue(Arrays.equals(defaultEntrySheetNames, entrySheet2.getEntryNames())),
+                () -> assertEquals(0, entrySheet2.getTotalPoints())
+        );
+    }
+}
