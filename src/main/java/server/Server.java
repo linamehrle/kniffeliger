ec7343adf50d26533830e920cfe58445ade3e33e
Riccardo Grieco <r.grieco@unibas.ch>
2024-03-20 22:54:13 +0100

    Removed main method from Server class

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,10 +8,6 @@ import server.networking.NetworkManagerServer;
 public class Server {
     NetworkManagerServer networkManager;
 
-    public static void main(String[] args) {
-        new Server(Integer.parseInt(args[0]));
-    }
-
     public Server(int port) {
         networkManager = new NetworkManagerServer(port);
     }
