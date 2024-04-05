0c6c17a15ea59403e6b87bbafdab7c37e8ec7bd6
linamehrle <lina.mehrle@unibas.ch>
2024-04-05 14:57:28 +0200

    Added basic layout for the game window

---
+++ src/main/java/client/gui/GameWindowController.java
@@ -0,0 +1,57 @@
+package client.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+public class GameWindowController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void saveDie1(ActionEvent event) {
+    }
+
+    public void saveDie2(ActionEvent event) {
+    }
+
+    public void saveDie3(ActionEvent event) {
+    }
+
+    public void saveDie4(ActionEvent event) {
+    }
+
+    public void saveDie5(ActionEvent event) {
+    }
+
+    public void stealEntryAction(ActionEvent event) {
+    }
+
+    public void freezeEntryButton(ActionEvent event) {
+    }
+
+    public void rotateSheetsAction(ActionEvent event) {
+    }
+
+    public void swapSheetsAction(ActionEvent event) {
+    }
+
+    public void deleteEntryAction(ActionEvent event) {
+    }
+
+    public void startGameAction(ActionEvent event) {
+    }
+
+    public void leaveGameAction(ActionEvent event) {
+    }
+
+    public void leaveLobbyAction(ActionEvent event) {
+    }
+
+    public void enterToEntrySheetAction(ActionEvent event) {
+    }
+}
