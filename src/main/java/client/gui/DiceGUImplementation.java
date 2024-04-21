e9d9ab547fc85b47cb4d0cbdbae1aa3c2687a529
Dominique Ostermayer <dominique.ostermayer@stud.unibas.ch>
2024-04-21 13:05:45 +0200

    new class to keep track of dice status in GUI, might eventually be replaced by dice class of GameLogic or other data structure

--- src/main/java/client/gui/DiceGUImplementation.java
+++ src/main/java/client/gui/DiceGUImplementation.java
@@ -1,2 +1,23 @@
-package client.gui;public class DiceGUImplementation {
+package client.gui;
+
+/**
+ * Class to implement dice in GUI, might eventually be replaced by dice class of Game Logic
+ */
+public class DiceGUImplementation {
+    private int diceID;
+    private boolean savingStatus;
+    private int diceValue;
+
+
+    public DiceGUImplementation(int diceID) {
+        this.diceID = diceID;
+        this.savingStatus = false;
+        this.diceValue = 0;
+    }
+
+    public int getID(){return diceID;}
+
+    public boolean getSavingStatus(){return savingStatus;}
+
+    public int getDiceValue(){return diceValue;}
 }
