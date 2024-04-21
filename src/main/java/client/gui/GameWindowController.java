5fd7a31273c339a68c3704230b113791b103525e
Dominique Ostermayer <dominique.ostermayer@stud.unibas.ch>
2024-04-21 13:04:42 +0200

    implemented pictures for dice and loading of dice into ListView

--- src/main/java/client/gui/GameWindowController.java
+++ src/main/java/client/gui/GameWindowController.java
@@ -1,7 +1,14 @@
 package client.gui;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.IntStream;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -10,11 +17,15 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import client.networking.ClientOutput;
 import client.networking.CommandsClientToServer;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.Logger;
 import starter.Starter;
 
@@ -50,9 +61,26 @@ public class GameWindowController implements Initializable {
     private ListView<String> entrySheet;
     @FXML
     private ListView<String> dice;
+    @FXML
+    private ListView<DiceGUImplementation> diceBox;
+    @FXML
+    private VBox diceBox1;
+    @FXML
+    private VBox diceBox2;
+    @FXML
+    private VBox diceBox3;
+    @FXML
+    private VBox diceBox4;
+    @FXML
+    private VBox diceBox5;
+
 
     private static ObservableList<String> entryList = FXCollections.observableArrayList();
-    private static ObservableList<String> diceList = FXCollections.observableArrayList();
+    private static ObservableList<DiceGUImplementation> diceList = FXCollections.observableArrayList();
+    //variables for dice images
+    private static Image[]  diceFaces = new Image[7];
+
+
 
     /**
      * The initialize function for the Game Window, it sets everything up.
@@ -75,11 +103,14 @@ public class GameWindowController implements Initializable {
         entrySheet.setItems(entryList);
 
         //würfel am anfang auf null
-        diceList.addAll("0", "0", "0", "0", "0");
-        dice.setItems(diceList);
+        diceList.addAll(new DiceGUImplementation[]{new DiceGUImplementation(1), new DiceGUImplementation(2), new DiceGUImplementation(3), new DiceGUImplementation(4), new DiceGUImplementation(5) });
+        diceBox.setItems(diceList);
+
+
+
 
         entrySheet.setDisable(true);
-        dice.setDisable(true);
+        //dice.setDisable(false);
         //diceTextField.setDisable(true);
 
         stealButton.setDisable(true);
@@ -100,6 +131,52 @@ public class GameWindowController implements Initializable {
                 }
             }
         });
+
+
+        //Load images
+        IntStream.range(0, diceFaces.length).forEach(i -> {
+            try {
+                diceFaces[i] = diceImageLoader(i );
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+        //cell factory for diceBox selection box
+        diceBox.setCellFactory(param -> new ListCell<DiceGUImplementation>() {
+            private String diceID;
+            private int diceValue;
+            private boolean empty;
+
+            @Override
+            public void updateItem(DiceGUImplementation dice, boolean empty) {
+                super.updateItem(dice, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    //set baseIndex, such that different symbols are loaded for saved and unsaved dice
+                    int baseIndex = 0;
+                    if (!dice.getSavingStatus()) {
+                        baseIndex = 6;}
+                    ImageView imageView = new ImageView();
+                    switch (dice.getDiceValue()) {
+                        case 1 -> imageView.setImage(GameWindowController.diceFaces[baseIndex + 1]);
+                        case 2 -> imageView.setImage(GameWindowController.diceFaces[baseIndex + 2]);
+                        case 3 -> imageView.setImage(GameWindowController.diceFaces[baseIndex + 3]);
+                        case 4 -> imageView.setImage(GameWindowController.diceFaces[baseIndex + 4]);
+                        case 5 -> imageView.setImage(GameWindowController.diceFaces[baseIndex + 5]);
+                        case 6 -> imageView.setImage(GameWindowController.diceFaces[baseIndex + 6]);
+                        default -> imageView.setImage(GameWindowController.diceFaces[0]);
+                    };
+
+                    setText(String.valueOf(dice.getID()));
+                    setGraphic(imageView);
+                }
+            }
+        });
+
 
     }
 
@@ -185,5 +262,43 @@ public class GameWindowController implements Initializable {
      */
     public void saveDiceAction(ActionEvent event) {
         //TODO
+    }
+
+
+    public void rollActionSend(ActionEvent event){
+        ClientOutput.sendToServer("GAME " + "roll" );
+    }
+
+    /*
+        Dice controls
+     */
+    /**
+     * method to load images of dice faces
+     * @param diceNumber number of dice face e.g. 1-6
+     * @return ImageView object of diceFace
+     * @throws FileNotFoundException
+     */
+    public static Image diceImageLoader(int diceNumber) throws FileNotFoundException {
+        //String.valueOf(GameWindowController.class.getResource("/images/dice-" + diceNumber  + ".png"))
+        //FileInputStream fis = new FileInputStream(file);
+        return new Image(String.valueOf(GameWindowController.class.getResource("/images/dice-" + diceNumber  + ".png")), 64, 63.2, true, false);
+    }
+
+    public void setDiceImage(HBox diceBoxID, int diceValue) {
+        ImageView imageView = new ImageView();
+        switch (diceValue) {
+            case 1 -> imageView.setImage(GameWindowController.diceFaces[1]);
+            case 2 -> imageView.setImage(GameWindowController.diceFaces[2]);
+            case 3 -> imageView.setImage(GameWindowController.diceFaces[3]);
+            case 4 -> imageView.setImage(GameWindowController.diceFaces[4]);
+            case 5 -> imageView.setImage(GameWindowController.diceFaces[5]);
+            case 6 -> imageView.setImage(GameWindowController.diceFaces[6]);
+            default -> imageView.setImage(GameWindowController.diceFaces[0]);
+        };
+
+    }
+
+    public void saveDiceClick(){
+
     }
 }
