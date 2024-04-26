3957cb585de246027ece24ad4bbd0668690f1209
Riccardo Grieco <r.grieco@unibas.ch>
2024-04-26 22:30:36 +0200

    Added test for Entry class

--- src/test/java/server/gamelogic/EntryTest.java
+++ src/test/java/server/gamelogic/EntryTest.java
@@ -9,6 +9,33 @@ class EntryTest {
     Entry entry1 = new Entry("ones", 0);
     Entry entry2 = new Entry("twos", 0);
 
+
+    @Test
+    @DisplayName("Checks if name is correctly returned by methods.")
+    void getNameTest(){
+        assertAll(() -> assertEquals("ones", entry1.getName()),
+                () -> assertEquals("twos", entry2.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("Check if final can be set.")
+    void  setFinalTest(){
+        entry1.setFinal();
+        assertAll(() -> assertTrue(entry1.getIsFinal()),
+                () -> assertFalse(entry2.getIsFinal())
+        );
+    }
+
+    @Test
+    @DisplayName("Check if final can be set.")
+    void  setFrozenStatus(){
+        entry1.setFinal();
+        assertAll(() -> assertTrue(entry1.getIsFinal()),
+                () -> assertFalse(entry2.getIsFinal())
+        );
+    }
+
     @Test
     @DisplayName("Checks if value can be set.")
     void setValueTest(){
@@ -17,14 +44,6 @@ class EntryTest {
                 () -> assertEquals(6, entry2.getValue())
         );
 
-    }
-
-    @Test
-    @DisplayName("Checks if name is correctly returned by methods.")
-    void getNameTest(){
-        assertAll(() -> assertEquals("ones", entry1.getName()),
-                () -> assertEquals("twos", entry2.getName())
-        );
     }
 
 }
