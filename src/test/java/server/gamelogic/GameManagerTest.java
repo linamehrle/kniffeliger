5f41220a2a21c0776ca928b0b4943fee365b912f
Riccardo Grieco <r.grieco@unibas.ch>
2024-04-28 16:39:07 +0200

    Adjusted deleteActionDiceTest

--- src/test/java/server/gamelogic/GameManagerTest.java
+++ src/test/java/server/gamelogic/GameManagerTest.java
@@ -7,11 +7,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class GameManagerTest {
+    // starter method cannot be tested with unit tests but only with play-through
 
-    //TODO: DO ALL THE TESTING
-    //TODO: reset dice testing
-    //TODO: check for action dice testing
-    //TODO: test delete action dice
+    /**
+     * Dummy player class to test methods. Contains all important fields and methods of a player.
+     */
+    public class DummyPlayer extends server.Player{
+        private ActionDice[] actionDice;
+
+        /**
+         * The constructor for the Player class. It starts a new ClientThread per Player.
+         */
+        public DummyPlayer(String name) {
+            super(name);
+        }
+
+        /**
+         * Get the action dice of player.
+         *
+         * @return all action dice of player saved in array.
+         */
+        public ActionDice[] getActionDice() {
+            return actionDice;
+        }
+
+        /**
+         * Access private field username.
+         *
+         * @return name of user
+         */
+        public String getUsername() {
+            return super.username;
+        }
+
+        /**
+         * Access private field id.
+         *
+         * @return id of user
+         */
+        public int getId() {
+            return super.id;
+        }
+
+        /**
+         * Set a new set of action dice.
+         *
+         * @param newActionDice new array of action dice a player can use.
+         */
+        public void setActionDices(ActionDice[] newActionDice) {
+            actionDice = newActionDice;
+        }
+
+    }
 
     /**
      * Turns action dice array into String array with names of actions as array
@@ -29,37 +76,13 @@ class GameManagerTest {
         return result;
     }
 
+
+//    // TODO: allDiceSavedTest
 //    @Test
-//    @DisplayName("Tests the method that deletes an action dice out of an array")
-//    void deleteActionDiceTest(){
-//        // action dice for player
-//        ActionDice[] actionDiceLina = new ActionDice[]{new ActionDice("steal"), new ActionDice("freeze"), new ActionDice("crossOut"), new ActionDice("shift"), new ActionDice("swap")};
-//        ActionDice[] actionDiceRiccardo = new ActionDice[]{new ActionDice("crossOut"), new ActionDice("shift"), new ActionDice("swap")};
+//    @DisplayName("Tests if it properly checks if all dice are saved.")
+//    void allDiceSavedTest() {
 //
-//        // players that hold action dice (needed to apply the delete entry method)
-//        Player lina = new Player("lina", 007);
-//        Player riccardo = new Player("riccardo", 001);
-//        lina.setActionDices(actionDiceLina);
-//        riccardo.setActionDices(actionDiceRiccardo);
-//
-//        // delete action dice
-//        GameManager.deleteActionDice(lina, "shift");
-//        GameManager.deleteActionDice(riccardo, "crossOut");
-//
-//        // control action dice to check
-//        ActionDice[] controlActionDiceLina = new ActionDice[]{new ActionDice("steal"), new ActionDice("freeze"), new ActionDice("crossOut"), new ActionDice("swap")};
-//        ActionDice[] controlActionDiceRiccardo = new ActionDice[]{new ActionDice("shift"), new ActionDice("swap")};
-//
-//
-//
-//        assertAll(() -> assertEquals(turnActionDiceToString(controlActionDiceLina), turnActionDiceToString(lina.getActionDice())),
-//                () -> assertEquals(turnActionDiceToString(controlActionDiceRiccardo), turnActionDiceToString(riccardo.getActionDice()))
-//        );
 //    }
-
-    // TODO: starterTest
-
-    // TODO: allDiceSavedTest
 
     // TODO: resetDiceTest
 
@@ -67,7 +90,32 @@ class GameManagerTest {
 
     // TODO: addActionDiceTest
 
-    // TODO: deleteActionDiceTest
+    @Test
+    @DisplayName("Tests the method that deletes an action dice out of an array")
+    void deleteActionDiceTest(){
+        // action dice for player
+        ActionDice[] actionDiceLina = new ActionDice[]{new ActionDice("steal"), new ActionDice("freeze"), new ActionDice("crossOut"), new ActionDice("shift"), new ActionDice("swap")};
+        ActionDice[] actionDiceRiccardo = new ActionDice[]{new ActionDice("crossOut"), new ActionDice("shift"), new ActionDice("swap")};
+
+        // players that hold action dice (needed to apply the delete entry method)
+        DummyPlayer lina = new DummyPlayer("lina");
+        DummyPlayer riccardo = new DummyPlayer("riccardo");
+        lina.setActionDices(actionDiceLina);
+        riccardo.setActionDices(actionDiceRiccardo);
+
+        // delete action dice
+        GameManager gm = new GameManager();
+        gm.deleteActionDice(lina, "shift");
+        gm.deleteActionDice(riccardo, "crossOut");
+
+        // control action dice to check
+        ActionDice[] controlActionDiceLina = new ActionDice[]{new ActionDice("steal"), new ActionDice("freeze"), new ActionDice("crossOut"), new ActionDice("swap")};
+        ActionDice[] controlActionDiceRiccardo = new ActionDice[]{new ActionDice("shift"), new ActionDice("swap")};
+
+        assertAll(() -> assertEquals(turnActionDiceToString(controlActionDiceLina), turnActionDiceToString(lina.getActionDice())),
+                () -> assertEquals(turnActionDiceToString(controlActionDiceRiccardo), turnActionDiceToString(riccardo.getActionDice()))
+        );
+    }
 
     // TODO: rankingTest
 
