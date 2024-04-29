b74d7ec6995d849777b5076a4a4e366a7f335368
Riccardo Grieco <r.grieco@unibas.ch>
2024-04-29 02:42:59 +0200

    Added logger statement in saving dices.

--- src/main/java/server/gamelogic/GameManager.java
+++ src/main/java/server/gamelogic/GameManager.java
@@ -169,11 +169,11 @@ public class GameManager implements Runnable {
 
                                 logger.log(gameLogic, "Save dices: " + Arrays.toString(savedDice));
 
-                                // TODO: SAVES DICE WITH NUMBER BY 0 1 2 3 4 (not like in terminal version with 1 2 3 4 5)
                                 // saves the rolled dice; if player does not want to save one, then "none" is sent
                                 if (!savedDice[1].equals("none")) {
                                     // turns the single String array entries into int and save the corresponding dice
-                                    for (int idx = 1; idx < savedDice.length - 1; idx++) {
+                                    for (int idx = 1; idx < savedDice.length; idx++) {
+                                        logger.trace("Save dice " + savedDice[idx]);
                                         int idxDice = Integer.parseInt(savedDice[idx]);
                                         allDice[idxDice].saveDice();
                                     }
